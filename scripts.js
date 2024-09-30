4743a03272b5549e874f6a73dd9b615248417cfe
Krizam Byanju <krizambyanju@gmail.com>
2024-09-30 23:26:52 +0545

    DarkMode/LightMode Web Page

---
+++ scripts.js
@@ -0,0 +1,61 @@
+// create a button in js bg color red and text click me
+// let Body = document.querySelector("h1")
+// let createButton = document.createElement("button");
+
+// createButton.innerHTML="Click Me";
+// createButton.style.backgroundColor ="Red";
+// Body.append(createButton);
+
+//  create a input and a button field using javascript and add a function that change color of the input when click the button 
+// let Body = document.querySelector("body");
+// let createButton = document.createElement("button");
+// createButton.innerHTML = "Click Me";
+// Body.prepend(createButton);
+// let createInput = document.createElement("input");
+// Body.prepend(createInput);
+
+
+// createButton.onclick = ()=>{
+//     createInput.style.color="red";
+// }
+
+
+
+// create a toggle button that changes the mode of screen to light and dark
+// let Body = document.querySelector("body");
+// let createButton = document.createElement("button");
+// createButton.innerHTML="Go To Dark Mode";
+// Body.prepend(createButton);
+
+// // createButton.onclick=()=>{
+// //     Body.style.backgroundColor ="black";
+
+// // }
+// let whiteMode = ()=>{
+//     Body.style.color = "white";
+//     createButton.innerHTML = "Go To White Mode";
+
+// }
+// onclick.whiteMode
+
+
+// let Body = document.querySelector("body");
+// let createButton = document.createElement("button");
+// Body.prepend(createButton);
+// createButton.innerHTML = "Go to Dark Mode";
+// let  darkMode =() =>{
+//     Body.style.backgroundColor = "black";   
+// }
+// let whiteMode = ()=>{
+    
+//     createButton.innerHTML = "Go to White Mode";
+//     Body.style.backgroundColor="white";
+    
+
+// }
+// createButton.addEventListener("click",whiteMode);
+// createButton.addEventListener("click",darkMode);
+
+
+
+
